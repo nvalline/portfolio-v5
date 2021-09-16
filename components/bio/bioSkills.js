e69611c9c8068f2d215cb59474d7c27d3a4fc12b
vall4251n <58278138+nvalline@users.noreply.github.com>
2021-09-15 22:25:58 -0600

    created skills data file

--- components/bio/bioSkills.js
+++ components/bio/bioSkills.js
@@ -1,14 +1,13 @@
 import bioStyles from '../../styles/Bio.module.scss';
+import { skills } from '../../data/skills';
 
 function bioSkills() {
-	const skills = ['React', 'Next', 'Javascript', 'Sass', 'Html', 'Css', 'Git'];
-
 	return (
 		<div className={bioStyles.bioSkills}>
 			<h3>Skills</h3>
 			<div className={bioStyles.skillBtns}>
 				{skills.map((skill) => {
-					return <h4>{skill}</h4>;
+					return <h4 key={skill.id}>{skill.skill}</h4>;
 				})}
 			</div>
 		</div>
---
+++ data/skills.js
@@ -0,0 +1,30 @@
+export const skills = [
+	{
+		id: 1,
+		skill: 'React'
+	},
+	{
+		id: 2,
+		skill: 'Next'
+	},
+	{
+		id: 3,
+		skill: 'Javascript'
+	},
+	{
+		id: 4,
+		skill: 'Sass'
+	},
+	{
+		id: 5,
+		skill: 'Html'
+	},
+	{
+		id: 6,
+		skill: 'Css'
+	},
+	{
+		id: 7,
+		skill: 'Git'
+	}
+];
