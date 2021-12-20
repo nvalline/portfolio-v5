00bf07946e3fd81eedd18a697a8d86433f0424fa
vall4251n <58278138+nvalline@users.noreply.github.com>
2021-12-20 12:56:21 -0700

    updated project data

--- data/projects.js
+++ data/projects.js
@@ -1,46 +1,6 @@
 export const projects = [
 	{
 		id: 1,
-		title: 'Note Taker',
-		description:
-			'This application allows the user to enter and save notes, view previous notes and delete notes that are no longer needed.',
-		link: 'https://peaceful-meadow-53544.herokuapp.com/',
-		img: '/images/projectImg/note-taker.png'
-	},
-	{
-		id: 2,
-		title: 'Day Planner',
-		description:
-			'This application allows the user to plan out their work day to stay ahead of the game.',
-		link: 'https://nvalline.github.io/day-planner/',
-		img: '/images/projectImg/dayplanner.png'
-	},
-	{
-		id: 3,
-		title: 'Weather Dashboard',
-		description:
-			'This application allows the user to stay up-to-date on the current weather & the forecast for the cities around the nation.',
-		link: 'https://nvalline.github.io/weather-dashboard/',
-		img: '/images/projectImg/weatherdashboard.png'
-	},
-	{
-		id: 4,
-		title: 'Eat-Da-Burger',
-		description:
-			'This application allows the user to log different hamburgers that the user would like to eat and also update the log when the burger has been eaten.',
-		link: 'https://powerful-springs-45034.herokuapp.com/',
-		img: '/images/projectImg/eat-da-burger.png'
-	},
-	{
-		id: 5,
-		title: 'Cookbook',
-		description:
-			'Cookbook is a community driven recipe database, which gives users the ability to sign-up, submit recipes, search for community recipes, and search a larger 3rd party database.',
-		link: 'https://the-cookbook-app.herokuapp.com/',
-		img: '/images/projectImg/cookbook-screenshot.png'
-	},
-	{
-		id: 6,
 		title: 'Earthquake Now',
 		description:
 			'This application allows the user to quickly find up-to-date seismic data and related news articles for a specified location. This provides the user with valuable information to make informed decisions.',
@@ -48,7 +8,7 @@ export const projects = [
 		img: '/images/projectImg/earthquakenow.png'
 	},
 	{
-		id: 7,
+		id: 2,
 		title: 'COVIDashboard',
 		description:
 			'COVIDashboard is an individualized dashboard providing personal contact tracing history and testing and patient data related to the COVID-19 pandemic.',
@@ -56,7 +16,15 @@ export const projects = [
 		img: '/images/projectImg/covidashboard.png'
 	},
 	{
-		id: 8,
+		id: 3,
+		title: 'NV Marketing, LLC',
+		description:
+			'NV Marketing, LLC is a custom Next.js website.  This site was designed and developed in-house to deliver its visitors a visually pleasing and performant experience.',
+		link: 'https://nv-marketing.com/',
+		img: '/images/projectImg/nv_marketing.png'
+	},
+	{
+		id: 4,
 		title: 'Nova Art Restoration',
 		description:
 			'Nova Art Restoration is a custom Wordpress website for an art resetoration company.  Custom CSS was added to the theme to provide the client with the look and feel desired.',
