f43b113cd30d3428a0c519b8ac2877b2a0b26857
vall4251n <58278138+nvalline@users.noreply.github.com>
2021-12-22 16:11:54 -0700

    added some seo meta tags

--- pages/index.js
+++ pages/index.js
@@ -9,11 +9,39 @@ import Work from '../components/work/work';
 import Contact from '../components/contact/contact';
 
 export default function Home({ projects, skills }) {
+	// const metaDescription =
+	// 	'NV Marketing, LLC provides website design services for small business. Website design packages include design & development and SEO to increase traffic';
+
 	return (
 		<>
 			<Head>
 				{/* Page Title */}
 				<title>Nate Valline | Web Developer</title>
+				{/* Page Description */}
+				<meta name='description' content={metaDescription} />
+				<meta
+					name='keywords'
+					content='website designer, website design, website developer, website developer, seo, freelance, web builder'
+				/>
+				{/* Open Graph Protocol */}
+				<meta property='og:title' content='Nate Valline' />
+				<meta property='og:type' content='website' />
+				{/* <meta
+					name='og:image'
+					property='og:image'
+					content='https://nv-marketing.com/images/og/og_website_image_fb.jpg'
+				/>
+				<meta property='og:url' content='https://nv-marketing.com' /> */}
+				<meta property='og:description' content={metaDescription} />
+				{/* Twitter Card */}
+				<meta name='twitter:card' content='summary_large_image' />
+				<meta name='twitter:creator' content='@Nate_Valline' />
+				<meta name='twitter:title' content='Nate Valline' />
+				<meta name='twitter:description' content={metaDescription} />
+				{/* <meta
+					name='twitter:image'
+					content='https://nv-marketing.com/images/og/og_website_image_twitter.png'
+				/> */}
 			</Head>
 
 			{/* Hero Section */}
